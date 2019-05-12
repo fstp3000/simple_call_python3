211fd234983bba156a21831f456e110730f4f489
Fstp <Fstp@FstpdeMacBook-Pro.local>
2019-05-12 22:23:47 +0800

    110

--- call.py
+++ call.py
@@ -4,4 +4,5 @@ if __name__=="__main__":
     #web3 initialize
     ethereum = farm()
     #find all machineID you specified
+    #machineID could be None, it will show all the photos
     ethereum.call_by_machineID(machineID = 10)
